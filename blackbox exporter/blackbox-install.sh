5b91157ca267a30ea8b5418acb23b52c1930dfde
Kovalev Vladislav <vladoz77@yandex.ru>
2025-12-29 09:25:11 +0300

    fix install script

--- blackbox exporter/blackbox-install.sh
+++ blackbox exporter/blackbox-install.sh
@@ -3,16 +3,24 @@
 # Script to install Docker and run blackbox-exporter with restricted access
 set -e  
 
-# IP, с которого разрешаем доступ
+# config
 ALLOWED_IP="172.16.10.1"
+BASE_DIR="/opt/blackbox-exporter"
+CERTS_DIR="$BASE_DIR/certs"
+IMAGE="prom/blackbox-exporter"
+CONTAINER_NAME="blackbox-exporter"
+VERSION="latest"
+PORT="9115"
 
-# Проверка, что скрипт запущен от root
+
+
+# check for root privileges
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root or use sudo"
   exit 1
 fi
 
-# Установка Docker, если не установлен
+# Install docker if not install
 if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Installing Docker..."
     curl -fsSL https://get.docker.com -o get-docker.sh
@@ -21,17 +29,35 @@ else
     echo "Docker is already installed."
 fi
 
-# Запуск Docker
+# Start docker service
 echo "Starting Docker service..."
 systemctl enable --now docker
 
-# Создание директории для конфигурации blackbox-exporter
-echo "Creating directory for blackbox-exporter configuration..."
-mkdir -p ~/blackbox-exporter
+# Creation of directories for blackbox-exporter configuration
+echo "Creating directory ${BASE_DIR} and ${CERTS_DIR} for blackbox-exporter configuration..."
+mkdir -p $BASE_DIR $CERTS_DIR
 
-# Создание конфигурационного файла
+# Generation of self-signed SSL certificates
+echo "Generating self-signed SSL certificates..."
+if [ -f "$CERTS_DIR/blackbox.crt" ] || [ -f "$CERTS_DIR/blackbox.key" ]; then
+    echo "SSL certificates already exist. Skipping generation."
+else
+    echo "SSL certificates do not exist. Generating new ones."
+    openssl req -newkey rsa:4096 \
+      -x509 \
+      -sha256 \
+      -days 3650 \
+      -nodes \
+      -out blackbox.crt \
+      -keyout blackbox.key \
+      -subj "/C=RU/L=Ryazan/O=Fabrika/OU=IT/CN=blackbox.home.local"
+    # Move generated certificates to the certs directory
+    mv blackbox.crt blackbox.key $CERTS_DIR/
+fi
+
+  # Creation of blackbox-exporter configuration file
 echo "Creating blackbox-exporter configuration file..."
-cat > ~/blackbox-exporter/blackbox.yml << EOF
+cat > $BASE_DIR/blackbox.yml << 'EOF'
 modules:
   https_endpoint:
     prober: http
@@ -47,19 +73,38 @@ modules:
       preferred_ip_protocol: ip4
 EOF
 
-# Запуск контейнера blackbox-exporter
-if [ "$(docker ps -q -f name=blackbox-exporter)" ]; then
-    echo "blackbox-exporter container is already running."
-else
-    echo "Running blackbox-exporter container..."
-    docker run -d --name blackbox-exporter \
-      -p 9115:9115 \
-      -v ~/blackbox-exporter/blackbox.yml:/etc/blackbox_exporter/blackbox.yml \
-      prom/blackbox-exporter --config.file=/etc/blackbox_exporter/blackbox.yml
+# Create blackbox web configuration with TLS
+cat > $BASE_DIR/web-config.yml << 'EOF'
+tls_server_config:
+  cert_file: /certs/blackbox.crt
+  key_file: /certs/blackbox.key
+
+basic_auth_users:
+  blackbox: $2y$10$1KWGGVB0jhe878t6tUvtYeB8/iudISI9tKHdsfSRMMxFKOIwg9XvC
+EOF
+
+
+# Check if blackbox-exporter container is already running delete if exists
+echo "Checking if $CONTAINER_NAME container is running..."
+EXISTING=$(docker ps -a -q -f name="^${CONTAINER_NAME}$")
+if [ "$EXISTING" ]; then
+    echo "$CONTAINER_NAME container is already running, removing it..."
+    docker rm -f "$CONTAINER_NAME"
 fi
+
+# Run blackbox-exporter container
+echo "Running $CONTAINER_NAME container..."
+docker run -d --name $CONTAINER_NAME \
+  -p $PORT:$PORT \
+  -v $BASE_DIR/blackbox.yml:/etc/blackbox_exporter/blackbox.yml \
+  -v $BASE_DIR/web-config.yml:/etc/blackbox_exporter/web-config.yml \
+  -v $CERTS_DIR:/certs \
+  $IMAGE:$VERSION \
+  --config.file=/etc/blackbox_exporter/blackbox.yml \
+  --web.config.file=/etc/blackbox_exporter/web-config.yml
 echo "blackbox-exporter is running."
 
-# Настройка iptables для ограничения доступа
+# Configure iptables to restrict access
 echo "Configuring iptables to allow only $ALLOWED_IP..."
 
 iptables -P INPUT DROP
