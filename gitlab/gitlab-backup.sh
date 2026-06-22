5c0c973d1890722d8a495bf97c2db2ec0138101a
Vladislav Kovalev <vladoz77@yandex.ru>
2026-06-22 14:51:51 +0300

    add gitlab-backup.sh

--- gitlab/gitlab-backup.sh
+++ gitlab/gitlab-backup.sh
@@ -4,9 +4,28 @@ set -a
 source .env   
 set +a  
 
+# Check required environment variables
+required_vars=(
+  AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY
+  RESTIC_PASSWORD
+  RESTIC_REPOSITORY
+  GITLAB_BACKUP_DIR
+)
+
+# Check required environment variables
+echo "Checking required environment variables..."
+for var in "${required_vars[@]}"; do
+  if [[ ! -v $var ]]; then
+    echo "Missing required variable: $var"
+    exit 1
+  fi
+done
+echo "All required variables are set"
+
 # Create backups directory if it doesn't exist
 echo "Creating backup directories..."
-mkdir -p $BACKUP_DIR/{gitlab,gitlab-config,postgres,traefik,environment}
+mkdir -p $GITLAB_BACKUP_DIR/{gitlab,gitlab-config,environment}
 
 # Backup gitlab data 
 echo "Creating GitLab backup..."
@@ -14,36 +33,25 @@ docker compose exec -T gitlab gitlab-backup create
 
 # Backup gitlab configuration files
 echo "Creating GitLab configuration backup..."
-tar -czf "$BACKUP_DIR/gitlab-config/gitlab-${TIMESTAMP}.tar.gz" \
+tar -czf "$GITLAB_BACKUP_DIR/gitlab-config/gitlab-${TIMESTAMP}.tar.gz" \
   ./gitlab/config/gitlab.rb \
   ./gitlab/config/gitlab-secrets.json
 
 # Backup .env file
 echo "Creating .env backup..."
-cp .env "$BACKUP_DIR/environment/env-${TIMESTAMP}.backup"
+cp .env "$GITLAB_BACKUP_DIR/environment/env-${TIMESTAMP}.backup"
 
-# Backup postgres database
-echo "Creating PostgreSQL backup..."
-docker compose exec -T postgres pg_dump \
-  -U "$DB_USERNAME" \
-  -d "$DB_NAME" \
-  -Fc \
-  > "$BACKUP_DIR/postgres/gitlab-db-${TIMESTAMP}.dump"
+echo "Backup completed successfully. Backup files are stored in $GITLAB_BACKUP_DIR."
 
-# Backup traefik configuration files
-echo "Creating Traefik configuration backup..."
-tar -czf "$BACKUP_DIR/traefik/traefik-${TIMESTAMP}.tar.gz" \
-  ./traefik/traefik.yml \
-  ./traefik/acme.json
-
-echo "Backup completed successfully. Backup files are stored in $BACKUP_DIR."
-
-# Backup $BACKUP_DIR to s3 with restic
+# Backup $GITLAB_BACKUP_DIR to s3 with restic
 restic_cmd() {
   docker run --rm \
     --hostname restic-backup \
-    --env-file .env \
-    -v "$(realpath ${BACKUP_DIR}):/data:ro" \
+    -e RESTIC_REPOSITORY \
+    -e RESTIC_PASSWORD \
+    -e AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY \
+    -v "$(realpath ${GITLAB_BACKUP_DIR}):/data:ro" \
     ghcr.io/restic/restic:latest \
     "$@"
 }
@@ -53,8 +61,8 @@ if ! restic_cmd snapshots > /dev/null 2>&1; then
   restic_cmd init
 fi
 
-# Backup $BACKUP_DIR to restic repository
-echo "Backing up $BACKUP_DIR to restic repository..."
+# Backup $GITLAB_BACKUP_DIR to restic repository
+echo "Backing up $GITLAB_BACKUP_DIR to restic repository..."
 restic_cmd backup /data --tag gitlab-backup
 
 # remove old backups from restic repository
@@ -69,5 +77,4 @@ echo "Restic backup completed successfully."
 
 # Remove backups older than BACKUP_RETENTION_DAYS
 echo "Removing backups older than $BACKUP_RETENTION_DAYS days..."
-find "$BACKUP_DIR" -type f -mtime +"$BACKUP_RETENTION_DAYS" -exec rm -f {} \;
-
+find "$GITLAB_BACKUP_DIR" -type f -mtime +"$BACKUP_RETENTION_DAYS" -exec rm -f {} \;
--- gitlab/restore-gitlab.sh
+++ gitlab/restore-gitlab.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 
-BACKUP_DIR="./backups"
+set -a        
+source .env   
+set +a  
+
+RESTORE_DIR="./restore"
+
+# Create restore directory if it doesn't exist
+echo "Creating restore directory..."
+mkdir -p "$RESTORE_DIR"
+
+# Restore $RESTORE_DIR from s3 with restic
+restic_cmd() {
+  docker run --rm \
+    --hostname restic-backup \
+    --env-file .env \
+    -v "$(realpath ${RESTORE_DIR}):/data" \
+    ghcr.io/restic/restic:latest \
+    "$@"
+}
+
+restic_cmd restore latest --target /data --tag gitlab-backup
 
 echo "=== Choose GitLab backup ==="
-select GITLAB_BACKUP in "$BACKUP_DIR"/gitlab/*; do
+select GITLAB_BACKUP in "$RESTORE_DIR"/data/gitlab/*; do
   if [[ -n "$GITLAB_BACKUP" ]]; then
     break
   fi
@@ -12,30 +32,43 @@ select GITLAB_BACKUP in "$BACKUP_DIR"/gitlab/*; do
 done
 
 echo "=== Choose Config backup ==="
-select CONFIG_BACKUP in "$BACKUP_DIR"/gitlab-config/*; do
+select CONFIG_BACKUP in "$RESTORE_DIR"/data/gitlab-config/*; do
   if [[ -n "$CONFIG_BACKUP" ]]; then
     break
   fi
   echo "Invalid selection"
 done
 
+echo "=== Choose environment backup ==="
+select ENV_BACKUP in "$RESTORE_DIR"/data/environment/*; do
+  if [[ -n "$ENV_BACKUP" ]]; then
+    break
+  fi
+  echo "Invalid selection"
+done
+
+
 BACKUP_ID=$(basename "$GITLAB_BACKUP" | sed 's/_gitlab_backup\.tar$//')
 
 echo ""
 echo "GitLab backup : $GITLAB_BACKUP"
 echo "Config backup : $CONFIG_BACKUP"
+echo "Environment backup : $ENV_BACKUP"
 read -rp "Continue? [y/N]: " confirm
 [[ "$confirm" =~ ^[Yy]$ ]] || { echo "Cancelled."; exit 0; }
 
-echo "[1/3] Restoring config..."
+echo "Restoring config..."
 tar -xzf "$CONFIG_BACKUP" --strip-components=1 -C ./gitlab/config/
 
-echo "[2/3] Starting restore..."
+echo "Restoring environment variables..."
+cp "$ENV_BACKUP" .env
+
+echo "GitLab restore..."
 docker compose exec -T gitlab gitlab-ctl stop puma
 docker compose exec -T gitlab gitlab-ctl stop sidekiq
 docker compose exec -T gitlab gitlab-backup restore BACKUP="$BACKUP_ID" force=yes
 
-echo "[3/3] Restarting GitLab..."
+echo "Restarting GitLab..."
 docker compose exec -T gitlab gitlab-ctl reconfigure
 docker compose exec -T gitlab gitlab-ctl restart
 
